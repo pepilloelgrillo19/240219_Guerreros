9eb08ed2ee214ffdb1c3eb9a54446d348bd10708
pepilloelgrillo19 <157523378+pepilloelgrillo19@users.noreply.github.com>
2024-02-22 17:48:27 +0100

    Version con Navigatio bar, pero sin funcionar correctamente el botón Home

--- app/src/main/java/com/lpg/tareafragmentslpg/MainActivity.kt
+++ app/src/main/java/com/lpg/tareafragmentslpg/MainActivity.kt
@@ -2,10 +2,37 @@ package com.lpg.tareafragmentslpg
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.navigation.NavigationBarView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var navBar:NavigationBarView = findViewById(R.id.bottom_NavigationView)
+        navBar.setOnItemSelectedListener {  item ->
+        //NavigationBarView.OnItemSelectedListener { item ->
+            when(item.itemId) {
+                R.id.backButton -> {
+                    onBackPressed()
+                    true
+                }
+                R.id.homeButton -> {
+
+                    val blankFragment1 = BlankFragment1()
+
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.fragmentContainer, blankFragment1)
+                        .addToBackStack(null)
+                        .commit()
+
+                    true
+                }
+                else -> false
+            }
+        }
+
+
     }
+
 }
